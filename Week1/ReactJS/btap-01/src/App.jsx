9bf087905b5ab101ccd466fba78e71fa14249e16
H82M19\Student <Student>
2026-01-24 14:43:40 +0700

    feat: react baitap-1 reactjs

---
+++ Week1/ReactJS/btap-01/src/App.jsx
@@ -0,0 +1,13 @@
+import ProductCard from "./components/Card"
+
+
+function App() {
+
+  return (
+    <>
+     <ProductCard />
+    </>
+  )
+}
+
+export default App
---
+++ Week1/ReactJS/btap-01/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const ProductCard  = () => {
+    return <div className="p-2 bg-black">
+        <div className="bg-white w-50 mx-auto p-2 rounded-2xl shadow hover:-translate-y-4 transition">
+        <image src='' />
+        <p>Ten san pham</p>
+        <p>Gia</p>
+        <button>Add to card</button>
+        </div>
+    </div>
+}
+ 
+export default ProductCard ;
