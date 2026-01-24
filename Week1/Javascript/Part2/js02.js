01c136ed1c4698d509127304b1386ea65dd7938c
H82M19\Student <Student>
2026-01-24 13:53:57 +0700

    feat: done part2_js_weak1 js

--- Week1/Javascript/Part2/js02.js
+++ Week1/Javascript/Part2/js02.js
@@ -12,7 +12,7 @@
 ]
 
 
- const bmi = (weight, height) => {
+const bmi = (weight, height) => {
     return weight /  (height * height)  ;   
 }
 
@@ -21,7 +21,7 @@ const bmiRun = () => {
     const {weight : weight2 , height : height2, name :name2 } = person[1] ; 
     const bmi1 = Math.floor( bmi(weight ,height )) ; 
     const bmi2 = Math.floor(bmi(weight2 ,height2 )) ; 
-    let markHigherBMI = bmi1 > bmi2 ; 
+    let markHigherBMI = bmi1 > bmi2 ;  
     return markHigherBMI ? `${name} (${bmi1})  nang hon ${name2} (${bmi2})` :  `${name2} (${bmi2})  nang hon ${name} (${bmi1})`   ; 
 }
 console.log(bmiRun()) ; 
